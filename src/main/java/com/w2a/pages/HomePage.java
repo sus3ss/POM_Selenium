130ecdefba60f6d9f1a720893ba13e0ebdba5105
sus3ss <Skadaiahnamath@BLR-LAP-015.global.internal>
2021-02-26 10:23:45 +0530

    ss

--- src/main/java/com/w2a/pages/HomePage.java
+++ src/main/java/com/w2a/pages/HomePage.java
@@ -8,7 +8,8 @@ import com.w2a.base.Page;
 public class HomePage extends Page {
 	
 	public void gotoSignup() {
-		driver.findElement(By.cssSelector(".zh-signup`"));
+		//driver.findElement(By.cssSelector(".zh-signup"));
+		click("Signuplink_CSS");
 	}
     public LoginPage goToLogin() {
     	 
--- src/main/java/com/w2a/pages/ZohoAppPage.java
+++ src/main/java/com/w2a/pages/ZohoAppPage.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebDriver;
 import com.w2a.base.Page;
 import com.w2a.pages.crm.CRMHomePage;
 
-public class ZohoAppPage extends LoginPage{
+public class ZohoAppPage extends Page{
 	
 	public CRMHomePage gotoCRM() {
 		//driver.findElement(By.xpath("//div[contains(text(),'CRM')]")).click();
